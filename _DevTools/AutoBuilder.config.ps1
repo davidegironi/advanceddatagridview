87a93e40f5f40a322701ecfdaf94e87c3a1ac6a8
Davide Gironi <davide.gironi@gmail.com>
2019-08-22 21:41:18 +0200

    updated version

--- _DevTools/AutoBuilder.config.ps1
+++ _DevTools/AutoBuilder.config.ps1
@@ -6,7 +6,7 @@ $solutionName = "AdvancedDataGridView"
 $versionMajor = "1"
 $versionMinor = "1"
 $versionBuild = GetVersionBuild
-$versionRevision = "4"
+$versionRevision = "5"
 #build version number
 $assemblyVersion = GetVersion $versionMajor $versionMinor $versionBuild $versionRevision
 $fileVersion = $assemblyVersion
